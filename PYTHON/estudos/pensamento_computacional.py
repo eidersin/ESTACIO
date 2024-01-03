468dbe0a77803de03f61e57e1ba55bdcb56f3be6
eidersin <dersonts@gmail.com>
2024-01-03 12:24:58 -0300

    Update: organização

---
+++ PYTHON/estudos/pensamento_computacional.py
@@ -0,0 +1,30 @@
+# Pilares do pensamento computacional
+"""
+Apresentação:
+De forma geral, há quatro pilares ou habilidades necessárias para que possamos exercer o pensamento computacional,
+conforme ilustrado a seguir:
+
+ - Decomposição
+ - Abstração
+ - Reconhecimento de Padrões
+ - Algorítmos
+
+O objetivo é desenvolver um raciocínio mais estruturado e direto, o que pode auxiliar muito na solução de problemas
+complexos. Vamos entender cada um deles agora.
+
+Primeiro pilar - decomposição
+    A decomposição busca dividir um problema ou sistema complexo em partes menores, mais fáceis de compreender,
+    gerenciar e resolver. Quando deixamos de olhar para um sistema como um todo e estudamos cada uma de suas peças
+    menores, fica mais simples de compreender e buscar a solução desejada.
+
+Segundo pilar — abstração
+    Buscamos desconsiderar informações ou detalhes que não sejam imprescindíveis para resolver nosso problema
+
+Terceiro pilar - reconhecimento de padrões
+    Padrões são semelhanças ou características que alguns dos problemas compartilham entre si. O reconhecimento
+    dessas referências tem por objetivo aproveitar soluções já usadas no passado para lidar com uma nova dificuldade
+    que seja suficientemente similar.
+
+Quarto pilar — algoritmos
+    Pilar fundamental do pensamento computacional é a automação por meio de algoritmos.
+"""
