4618414750772c0ba5ffe1a3291f177cbaf4951e
Derson <dersonts@gmail.com>
2024-02-27 19:56:04 -0300

    Update Aula

--- PYTHON/estudos/Python em Outros Paradigmas.py
+++ PYTHON/estudos/Python em Outros Paradigmas.py
@@ -197,6 +197,62 @@ Boa prática de programação:
 
 
 """
+# # script principal.py
+# from threading import Thread
+# from multiprocessing import Process
+#
+# def funcao_a(nome):
+#     print(nome)
+#
+# def main():
+#     t = Thread(target=funcao_a, args=("Minha Thread",))
+#     t.start()
+#
+#     p = Process(target=funcao_a, args=("Meu Processo",))
+#     p.start()
+#
+# if __name__ == '__main__':
+#     main()
+
+# script processos.py
+from threading import Thread
+from multiprocessing import Process
+
+minha_lista = []
 
 
+def adiciona():
+    for i in range(100):
+        minha_lista.append(1)
 
+
+def main():
+    tarefas = []
+
+    for indice in range(10):
+        t = Thread(target=adiciona)
+        tarefas.append(t)
+        t.start()
+
+    for indice in range(10):
+        p = Process(target=adiciona)
+        tarefas.append(t)
+        p.start()
+
+    for tarefa in tarefas:
+        tarefa.join()
+
+    print(len(minha_lista))
+
+
+if __name__ == '__main__':
+    main()
+print("Antes de aguardar o termino!", len(minha_lista))
+
+for tarefa in minha_lista:
+    tarefa.join()
+
+print("Após aguardar o termino!", len(minha_lista))
+
+if __name__ == "__main__":
+    main()
